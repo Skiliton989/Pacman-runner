81bfa55f51ac26fea32522193aa4646d0120c440
Skiliton <skiliton989@gmail.com>
2021-11-10 15:05:31 +0500

    Создание регистраций, авторизации и личного кабинета пользователя

---
+++ pacman 2.0/index.php
@@ -0,0 +1,8 @@
+<?php
+include_once('header_not_autorization.php');
+
+?>
+		
+		<div class = 'center3'><a href="authorization/enter.php">Войти</a><br><a href = "check_in/check_in.php">Регистрация</a></div>
+	</body>
+</html>
